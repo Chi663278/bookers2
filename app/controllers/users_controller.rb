8b9551486f20167f700f0bca66006c5fbb0d6f4b
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-04-04 13:58:09 +0000

    [Update] 4-27 sign in, sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,12 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
+    @user = User.page(params[:id])
   end
 
   def show
+
   end
 
   def edit
