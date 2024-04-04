e2a726f3ad7ffdd59860a01e96c566d850aa1ca9
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-04-04 15:12:04 +0000

    [Update] 4-27 authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,17 @@ class UsersController < ApplicationController
 
   def update
   end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
