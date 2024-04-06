7ad7db90faaa55fe051e97a38dd868dc666a925f
Chi663278 <159125419+Chi663278@users.noreply.github.com>
2024-04-06 12:24:22 +0000

    [Update] 4-27 books/show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,15 @@
 class BooksController < ApplicationController
-
-  def new
-    @book = Book.new
-
-  end
-
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def edit
@@ -18,12 +22,20 @@ class BooksController < ApplicationController
   end
 
   def index
+    @user = current_user
     @book = Book.new
     @books = Book.all
-    @user = current_user
+
   end
 
   def show
-    @book = Book.find(params[:id])
+    @user = current_user
+    @book = Book.new
+    @books = Book.find(params[:id])
+  end
+
+  private #ストロングパラメータbook_paramsを定義
+  def book_params
+    params.require(:book).permit(:title, :caption, :image)
   end
 end
